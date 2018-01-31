ba15b41bd2350153da5b400a4777488eb442793b
Francois Best <francoisbest@gmail.com>
2018-01-31 08:54:36 +0100

    start: Initial commit
    Ejected from Apollo Playground https://launchpad.graphql.com/xnm7m1xxml

---
+++ src/schema.js
@@ -0,0 +1,151 @@
+// Welcome to Launchpad!
+// Log in to edit and save pads, run queries in GraphiQL on the right.
+// Click "Download" above to get a zip with a standalone Node.js server.
+// See docs and examples at https://github.com/apollographql/awesome-launchpad
+
+// graphql-tools combines a schema string with resolvers.
+import { makeExecutableSchema } from 'graphql-tools'
+import axios from 'axios'
+import moment from 'moment-timezone'
+// import typeDefs from './schema.graphql'
+
+const strava = axios.create({
+  baseURL: 'https://www.strava.com/api/v3'
+})
+const stravels = axios.create({
+  baseURL: 'https://wt-92cccbcf027a1b4070443ff04b9033cc-0.sandbox.auth0-extend.com/stravels-sandbox'
+})
+
+const injectHeaders = (context) => ({
+  headers: {
+    Authorization: context.headers.authorization
+  }
+})
+
+// Construct a schema, using GraphQL schema language
+const typeDefs = `
+  enum DISTANCE_UNIT {
+    METERS
+    KILOMETERS
+  }
+
+  type User {
+    id: ID! @unique
+    name: String!
+    profilePicture: String!
+    travels: [Travel!]!
+  }
+
+  type Activity {
+    id: ID! @unique
+    name: String!
+    date(tz: String): String!
+    distance(unit: DISTANCE_UNIT = METERS): Float!
+    elevation: Float!
+    polyline: String!
+  }
+
+  type Travel {
+    id: ID! @unique
+    name: String
+    author: User!
+    activities: [Activity!]!
+  }
+
+  type Query {
+    me: User
+    travel(id: ID!): Travel
+    activity(id: ID!): Activity
+    activities(last: Int): [Activity!]
+    publicTravels: [Travel!]!
+  }
+`
+
+const formatDate = (date) =>
+  ({ tz }) => tz ? moment(date).tz(tz).format() : date
+
+const formatActivity = (data) => ({
+  ...data,
+  date: formatDate(data.date || data.start_date),
+  distance: ({ unit }) => {
+    return data.distance * (unit === 'KILOMETERS' ? 0.001 : 1.0)
+  },
+  elevation: data.elevation || data.total_elevation_gain,
+  polyline: data.polyline || data.map.summary_polyline
+})
+
+// Provide resolver functions for your schema fields
+const resolvers = {
+  Query: {
+    async me (root, args, context) {
+      const { data } = await strava.get('/athlete', injectHeaders(context))
+      return {
+        id: data.id,
+        name: `${data.firstname} ${data.lastname}`,
+        profilePicture: data.profile,
+        travels: await stravels
+          .get(`/travels`, injectHeaders(context))
+          .then(res => res.data)
+          .then(travels => travels.map(t => ({
+            ...t,
+            activities: t.activities.map(formatActivity)
+          })))
+      }
+    },
+    async travel (root, args, context) {
+      if (!args.id) return null
+      return stravels.get(`/travels/${args.id}`, injectHeaders(context))
+        .then(res => res.data)
+        .then(travel => ({
+          ...travel,
+          activities: travel.activities.map(formatActivity)
+        }))
+    },
+    async activity (root, args, context) {
+      if (!args.id) return null
+      const { data } = await strava.get(`/activities/${args.id}`, injectHeaders(context))
+      return formatActivity(data)
+    },
+    async activities (root, args, context) {
+      const { data } = await strava.get('/athlete/activities', {
+        ...injectHeaders(context),
+        params: {
+          per_page: args.last
+        }
+      })
+      return data.map(formatActivity)
+    },
+    async publicTravels (root, args, context) {
+      return []
+    }
+  }
+}
+
+// Required: Export the GraphQL.js schema object as "schema"
+export const schema = makeExecutableSchema({
+  typeDefs,
+  resolvers
+})
+
+// Optional: Export a function to get context from the request. It accepts two
+// parameters - headers (lowercased http headers) and secrets (secrets defined
+// in secrets section). It must return an object (or a promise resolving to it).
+export function context (headers, secrets) {
+  return {
+    headers,
+    secrets
+  }
+};
+
+// Optional: Export a root value to be passed during execution
+// export const rootValue = {};
+
+// Optional: Export a root function, that returns root to be passed
+// during execution, accepting headers and secrets. It can return a
+// promise. rootFunction takes precedence over rootValue.
+// export function rootFunction(headers, secrets) {
+//   return {
+//     headers,
+//     secrets,
+//   };
+// };
---
+++ src/server.js
@@ -0,0 +1,48 @@
+import express from 'express'
+import { graphqlExpress } from 'apollo-server-express'
+import bodyParser from 'body-parser'
+import cors from 'cors'
+
+import * as Schema from './schema'
+
+const PORT = 3000
+const server = express()
+
+if (typeof process.env.APOLLO_ENGINE_KEY === 'undefined') {
+  console.warn('WARNING: process.env.APOLLO_ENGINE_KEY is not defined. Check README.md for more information')
+}
+
+let schema
+const schemaFunction = Schema.schemaFunction || (() => Schema.schema)
+const rootFunction = Schema.rootFunction || (() => schema.rootValue)
+const contextFunction = Schema.context || ((headers, secrets) => ({
+  headers,
+  ...secrets
+}))
+
+server.use(cors())
+
+server.use('/graphql', bodyParser.json(), graphqlExpress(async (request) => {
+  if (!schema) {
+    schema = schemaFunction(process.env)
+  }
+  const context = await contextFunction(request.headers, process.env)
+  const rootValue = await rootFunction(request.headers, process.env)
+
+  return {
+    schema: await schema,
+    rootValue,
+    context,
+    tracing: true
+  }
+}))
+
+// server.use('/graphiql', graphiqlExpress({
+//   endpointURL: '/graphql',
+//   query: ``
+// }))
+
+server.listen(PORT, () => {
+  console.log(`GraphQL Server is now running on http://localhost:${PORT}/graphql`)
+  console.log(`View GraphiQL at http://localhost:${PORT}/graphiql`)
+})
