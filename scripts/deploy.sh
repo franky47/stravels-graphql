aefe9791ab8839d66c505ba7414244630a550f04
Francois Best <francoisbest@gmail.com>
2018-02-06 23:39:32 +0100

    refactor: Move deployment script to own file

---
+++ scripts/deploy.sh
@@ -0,0 +1,5 @@
+now --public \
+  -e APOLLO_ENGINE_KEY=@stravels-graphql.apollo_engine_key \
+  -e DATABASE_URI=@stravels-graphql.database_uri  \
+  -e STRAVA_CLIENT_ID=@stravels-graphql.strava_client_id  \
+  -e STRAVA_CLIENT_SECRET=@stravels-graphql.strava_client_secret
