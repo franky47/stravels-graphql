1d8bee91852cafa0d6c39a0209f2c9f653350e7a
Francois Best <francoisbest@gmail.com>
2018-02-09 22:22:07 +0100

    fix: Use time-based pagination to simplify stitching

--- src/queries/activities.js
+++ src/queries/activities.js
@@ -1,9 +1,38 @@
 import strava from '../stravaApi'
-import { transformActivity, resolveActivity } from '../transforms'
+import {
+  transformActivity,
+  resolveActivity,
+  activityFilter
+} from '../transforms'
 
-export const getAllActivities = async (root, args, context) => {
-  const activities = await strava.getAllActivities(context.token)
+/**
+ * getActivities - Paginated endpoint for gettting Strava activity data.
+ *
+ * Pagination arguments:
+ *   before       ISO-8601 formatted date string. Will return activities that
+ *                happened before this date, exclusively.
+ *                Useful for fetching more data at the end of a scroll list view.
+ *                If not provided, it is considered equal to the Unix epoch.
+ *
+ *   after        ISO-8601 formatted date string. Will return activities that
+ *                happened after this date, exclusively.
+ *                Useful for getting the most up to date data.
+ *                If not provided, it is considered equal to current time.
+ */
+export const getActivities = async (_, { before, after }, context) => {
+  const options = {
+    before: before ? new Date(before).getTime() / 1000 : undefined,
+    after: after ? new Date(after).getTime() / 1000 : undefined
+  }
+  const activities = await strava.getActivities(context.token, options)
+  if (after) {
+    // From the Strava API docs:
+    // [Activities] will be sorted oldest first if the `after` parameter is used.
+    // -> if so, reverse the order to keep it consistent (newest first)
+    activities.reverse()
+  }
   return activities
+    .filter(activityFilter)
     .map(transformActivity)
     .map(resolveActivity)
 }
