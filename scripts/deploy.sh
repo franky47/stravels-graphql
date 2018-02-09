568237793615710e9adc26cce9a778a14546307d
Francois Best <francoisbest@gmail.com>
2018-02-09 17:44:13 +0100

    fix: Escape last line

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -1,7 +1,7 @@
 now --public \
   -e APOLLO_ENGINE_KEY=@stravels-graphql.apollo_engine_key \
-  -e DATABASE_URI=@stravels-graphql.database_uri  \
-  -e STRAVA_CLIENT_ID=@stravels-graphql.strava_client_id  \
-  -e STRAVA_CLIENT_SECRET=@stravels-graphql.strava_client_secret  \
-  -e SENTRY_DSN=@stravels-graphql.sentry_dsn
+  -e DATABASE_URI=@stravels-graphql.database_uri \
+  -e STRAVA_CLIENT_ID=@stravels-graphql.strava_client_id \
+  -e STRAVA_CLIENT_SECRET=@stravels-graphql.strava_client_secret \
+  -e SENTRY_DSN=@stravels-graphql.sentry_dsn \
   -e JWT_SECRET=@stravels-graphql.jwt_secret
