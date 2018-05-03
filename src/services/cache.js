9c48c1f970bba6b49d667d2453b9643b80af1fcb
Francois Best <francoisbest@gmail.com>
2018-05-03 18:05:07 +0200

    feat: Add Flow support

--- src/services/cache.js
+++ src/services/cache.js
@@ -1,46 +1,60 @@
+// @flow
 import ms from 'ms'
 
-const defaultOptions = {
-  ttl: '3s',
-  refreshRate: '1s'
+type Options = {
+  ttl: ms,
+  refreshRate: ms
+}
+
+type CacheItem = {
+  +value: any,
+  +time: number
 }
 
 export default class Cache {
-  constructor(_options = {}) {
-    const opt = { ...defaultOptions, ..._options }
+  // Member types
+  options: Options
+  state: Map<string, CacheItem>
+  timer: IntervalID
+
+  constructor({ ttl = '3s', refreshRate = '1s' }: Options = {}) {
     this.options = {
-      ttl: ms(opt.ttl),
-      refreshRate: ms(opt.refreshRate)
+      ttl: ms(ttl),
+      refreshRate: ms(refreshRate)
     }
-    this.state = {}
+    this.state = new Map()
     this.timer = setInterval(this._tick.bind(this), this.options.refreshRate)
   }
 
-  set(key, value) {
-    this.state[key] = {
+  // --
+
+  set(key: string, value: any) {
+    this.state.set(key, {
       value,
       time: Date.now()
-    }
+    })
     return value
   }
 
-  has(key) {
-    return this.state.hasOwnProperty(key)
+  has(key: string): boolean {
+    return this.state.has(key)
   }
 
-  get(key) {
-    return this.state[key].value
+  get(key: string): any {
+    return this.state.get(key)
   }
 
-  delete(key) {
-    delete this.state[key]
+  delete(key: string) {
+    this.state.delete(key)
   }
+
+  // Private --
 
   _tick() {
     const now = Date.now()
-    const stateCopy = { ...this.state }
-    Object.keys(stateCopy).forEach(key => {
-      const time = this.state[key].time + this.options.ttl
+    Array.from(this.state.keys()).forEach(key => {
+      const item = this.state.get(key)
+      const time = (item ? item.time : 0) + this.options.ttl
       if (time < now) {
         this.delete(key)
       }
